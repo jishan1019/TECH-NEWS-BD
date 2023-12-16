25780cd8f310263149e56288edc3cab143f88c94
jishan1019 <imranislamjishan200@gmail.com>
2023-12-16 23:51:59 +0600

    social icon add on details page

---
+++ js/singleNews.js
@@ -0,0 +1,41 @@
+const showSingleNews = () => {
+  const singleNews = JSON.parse(localStorage.getItem("singleNews"));
+  const detailsTitle = document.querySelector("#detailsTitle");
+  const fullDetails = document.querySelector("#fullDetails");
+
+  detailsTitle.innerHTML = `
+         <p class="text-black/40 text-xs">Home >${singleNews?.title}</p>
+          <h1 class="textSeconday my-3 text-2xl font-bold">${singleNews?.title}.</h1>
+          <p class="text-black/40 text-xs space-x-2">${singleNews?.catagory} - ${singleNews?.pubDate}</p>
+       
+      `;
+
+  fullDetails.innerHTML = `
+            <img class="lg:w-1/2 h-64 " src="${singleNews?.image}" alt="">
+            <h1 class="textSeconday my-3 text-2xl font-bold">${
+              singleNews?.description?.shortDes
+            }.</h1>
+
+           <div>
+            <a href=${singleNews?.websiteLink} target="_blank">${
+    singleNews?.websiteLink
+      ? `Website Link: <span class="underline textSeconday">${singleNews?.websiteLink}</span>`
+      : ""
+  }
+            </a>
+
+             <p class="text-sm mt-1">${
+               singleNews?.websiteLink
+                 ? "Click This link For more information."
+                 : ""
+             }</p>
+             </div>
+
+            <p>${singleNews?.description?.longDes}.</p>
+  
+      `;
+
+  console.log(singleNews, "from single details ");
+};
+
+showSingleNews();
--- js/topNews.js
+++ js/topNews.js
@@ -6,20 +6,21 @@ const fetchData = () => {
 
 const displayData = (data) => {
   const topNewsContainer = document.querySelector("#topNewsContainer");
-  console.log(data);
-
   data?.forEach((item) => {
     const newsCard = document.createElement("div");
     newsCard.innerHTML = `
     
-      <div class="card w-full h-72 shadow-xl rounded-sm relative">
+      <div onclick="fetchSingleNews(${item?.id})"  class="card w-full h-72 shadow-xl rounded-sm relative cursor-pointer	">
                 <figure>
                     <img class="w-full h-[40vh]" src="${item?.image}" alt="Shoes" />
                 </figure>
                 <div class="card-body absolute bottom-0 bg-black/50 py-2 w-full textPrimary ">
                     <h2 class="card-title">${item?.title}</h2>
-                    <p>${item?.pubDate}</p>
-
+                    
+                    <div class="flexContainer justify-between	">
+                    <p>Pub: ${item?.pubDate}</p>
+                    <button class="underline">Read More</button>
+                    </div>
                 </div>
             </div>
 
@@ -27,6 +28,17 @@ const displayData = (data) => {
 
     topNewsContainer.appendChild(newsCard);
   });
+};
+
+const fetchSingleNews = (singleItemId) => {
+  fetch("../data/topNews.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const singleNews = data?.find((news) => news?.id === singleItemId);
+      localStorage.setItem("singleNews", JSON.stringify(singleNews));
+    });
+
+  window.location.href = "newsDetails.html";
 };
 
 fetchData();
